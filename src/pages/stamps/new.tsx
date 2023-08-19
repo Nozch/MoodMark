bceb8100d7898647f743e1b797b83fe1ca7719bf
Nozch <freegiafrag@gmail.com>
2023-08-19 17:54:36 +0900

    arraged new stamps UI

--- src/pages/stamps/new.tsx
+++ src/pages/stamps/new.tsx
@@ -1,4 +1,4 @@
-import { Center, NumberInput, NumberIncrementStepper, NumberInputField, NumberInputStepper, NumberDecrementStepper, Button, VStack, Box, Input, Flex } from '@chakra-ui/react'
+import { Center, NumberInput, NumberIncrementStepper, NumberInputField, NumberInputStepper, NumberDecrementStepper, Button, VStack, Box, Input, Flex, Spacer } from '@chakra-ui/react'
 import { useForm, Controller } from "react-hook-form"
 import { trpc } from "@/utils/trpc"
 import { CreateStampInput } from "@/schema/stamp.schema"
@@ -20,12 +20,12 @@ function CreateStampPage() {
   const { control, handleSubmit, register, setValue } = useForm<CreateStampInput>()
 
   const router = useRouter()
+
   const { mutate, error } = trpc.useMutation(['stamps.create-stamp'], {
     onSuccess({ id }) {
       router.push(`/stamps/${id}`)
     }
   })
-
 
   function onSubmit(values: CreateStampInput) {
     values.price = Number(values.price)
@@ -44,76 +44,82 @@ function CreateStampPage() {
 
     setValue('gradient', value)
   }
-  
+
 
   return <form onSubmit={handleSubmit(onSubmit)}>
     {error && error.message}
     <Center>
-    <h1>Create Stamps</h1>
+      <h1>Create Stamps</h1>
     </Center>
     <Center>
-      <Flex flexDirection="column">
-        <Flex alignItems="center">
+      <Flex flexDirection="column" height="600px" position="relative">
+        <Flex position="absolute" top="20" left="2" justifyContent="center" alignItems="flex-start">
           <Input
-          type="color"
-          value={colors[0]}
-          {...register('color1')}
-          onChange={(e) => setColors([e.target.value, colors[1]])}
+            type="color"
+            value={colors[0]}
+            {...register('color1')}
+            onChange={(e) => setColors([e.target.value, colors[1]])}
           />
         </Flex>
-        <Flex alignItems="center">
-        <Input
-          type="color"
-          value={colors[1]}
-          {...register('color2')}
-          onChange={(e) => setColors([colors[0], e.target.value])}
-
+        <BezierCurveEditor
+          points={points}
+          value={points.control2.y}
+          handleSliderChange={handleSliderChange}
+          colors={colors}
         />
+        <Flex position="absolute" bottom="0" left="2" justifyContent="center" alignItems="flex-end">
+          <Input
+            type="color"
+            value={colors[1]}
+            {...register('color2')}
+            onChange={(e) => setColors([colors[0], e.target.value])}
+          />
+        </Flex>
       </Flex>
-      </Flex>
-      <BezierCurveEditor
-        points={points}
-        value={points.control2.y}
-        handleSliderChange={handleSliderChange}
-        colors={colors}
-      />
-      
     </Center>
     <input type="hidden" {...register('gradient')} value={points.control2.y} />
+
     <Center>
+      <Box mx={140}>
+      </Box>
       <Controller
         name="price"
         control={control}
         defaultValue={0}
-        render={({field: { onChange, onBlur, value }}) => (
+        render={({ field: { onChange, onBlur, value } }) => (
           <NumberInput
             min={0}
             max={100}
             value={value}
-            onChange={(valueAsString, valueAsNumber) => 
+            onChange={(valueAsString, valueAsNumber) =>
               onChange(valueAsNumber)}
             onBlur={onBlur}
+            width="80px"
           >
-            <NumberInputField placeholder="price of stamp"/>
-              <NumberInputStepper>
-                <NumberIncrementStepper />
-                <NumberDecrementStepper />
-              </NumberInputStepper>
+            <NumberInputField placeholder="price of stamp" />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
           </NumberInput>
         )}
-        />
-      </Center>
-      <VStack spacing={4}>
-      <Box>
-      
-      </Box>
-     <Box> 
-     </Box>
-    </VStack>
-      <Button colorScheme='teal' variant='outline'>Create</Button>
+      />
+    </Center>
 
+    <Box my={4}>
+    </Box>
+
+    <Center>
+      <Button
+        colorScheme='teal'
+        variant='outline'
+        type='submit'
+        width={300}
+      >Create
+      </Button>
+    </Center>
+    
   </form>
 }
-
 
 export default CreateStampPage
